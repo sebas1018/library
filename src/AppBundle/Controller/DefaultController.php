d3a580e6896c4ca56fc60980b8c7d115a2d85264
sebastian Rojas Quintero <sebaron0205@gmail.com>
2018-01-26 03:02:25 -0500

    creación de login

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -17,5 +20,24 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+
+    /**
+     * @Route("/password")
+     */
+    public function password(UserPasswordEncoderInterface $encoder)
+    {
+      $user = new User();
+    $plainPassword = 'ryanpass';
+    $encoded = $encoder->encodePassword($user, $plainPassword);
+        return new Response('<html><body>Password:'.$encoded.'</body></html>');
     }
 }
